9071b26ff04f55cedf6240de5f055ec0426d739d
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-04-16 09:43:37 +0600

    webcame is not showing

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import fractions
+from flask import Flask, Response, render_template
+from webcam import WebCam
 
 app = Flask(__name__)
 
@@ -6,9 +8,16 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route("/face-detection")
-def faceDetection():
-    return render_template('face.html')
+@app.route("/video")
+
+def video():
+    return Response(infiniteLoop(WebCam()), mimetype='multipart/x-mixed-replace; boundary = frame')
+
+def infiniteLoop(webcam):
+    while True:
+        frame = WebCam.get_frame()
+        yield(b'--frame\r\n'
+                    b'Content-type: image/jpeg\r\n\r\n' + frame +b'\r\n\r\n')
 
 if __name__ == "__main__":
     app.run(debug = True)
