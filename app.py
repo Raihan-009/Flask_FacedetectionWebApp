f73c6290eca63d8320fa9c1a6181985290c84cd6
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-04-18 23:20:45 +0600

    webcam is working

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-import fractions
 from flask import Flask, Response, render_template
-from webcam import WebCam
+import cv2
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index_page():
     return render_template('index.html')
 
-@app.route("/video")
+@app.route("/face_detection")
+def face_detection():
+    return render_template('face.html')
 
-def video():
-    return Response(infiniteLoop(WebCam()), mimetype='multipart/x-mixed-replace; boundary = frame')
+def generate_frames():
+    cam = cv2.VideoCapture(0)
 
-def infiniteLoop(webcam):
     while True:
-        frame = WebCam.get_frame()
-        yield(b'--frame\r\n'
-                    b'Content-type: image/jpeg\r\n\r\n' + frame +b'\r\n\r\n')
+        ret, frame = cam.read()
+        if ret:
+            frame = cv2.imencode('.jpg', frame)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            break
+        
+@app.route("/video")
+def video_streaming():
+
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug = True)
--- webcam.py
+++ webcam.py
@@ -1,14 +1,20 @@
 import cv2
+from flask import Response
 
-class WebCam():
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-    def __del__(self):
-        self.video.release()
-    def get_frame(self):
-        ret, frame = self.video.read()
+def generate_frames():
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cam.read()
         if ret:
-            ret, jpg = cv2.imencode('.jpg',frame)
-            
-        return jpg.tobytes()
+            frame = cv2.imencode('.jpg', frame)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            break
+
+def video_streaming():
+
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
         
+
+    
