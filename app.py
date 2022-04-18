0ed8fcd3eb914a8141250fb297effe9abb06a905
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-04-19 02:21:24 +0600

    facetracking web page is working

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response, render_template
 import cv2
+import faceTracker as ft
 
 
 app = Flask(__name__)
@@ -9,12 +10,22 @@ def index_page():
     return render_template('index.html')
 
 @app.route("/face_detection")
+def face():
+    return render_template('facetracking.html')
+
 def face_detection():
-    return render_template('face.html')
-
-def generate_frames():
+    tracker = ft.faceDetector()
     cam = cv2.VideoCapture(0)
-
+    while True:
+        ret, frame = cam.read()
+        if ret:
+            _ , frame = tracker.findFaces(frame, drawP=False)
+            frame = cv2.imencode('.jpg', frame)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            break
+def original_face():
+    cam = cv2.VideoCapture(0)
     while True:
         ret, frame = cam.read()
         if ret:
@@ -23,10 +34,12 @@ def generate_frames():
         else:
             break
         
-@app.route("/video")
-def video_streaming():
-
-    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route("/faceTracking")
+def faceTracking_streaming():
+    return Response(face_detection(),mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route("/originalface")
+def originalFace_streaming():
+    return Response(original_face(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug = True)
