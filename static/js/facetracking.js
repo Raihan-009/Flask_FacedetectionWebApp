76b53ec70efaeda323f12441038e0126be9d5b8b
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-04-20 00:22:41 +0600

    face tracking and streaming done

--- static/js/facetracking.js
+++ static/js/facetracking.js
@@ -1,7 +1,18 @@
-const content = document.querySelector(".content");
-console.log(content)
+let streaming = document.getElementById("streaming");
+let liveStreaming = document.getElementById("videoStreaming");
+liveStreaming.style.display = "none";
+
 const stopButton = document.querySelector(".stpbtn");
+const strtButton = document.querySelector(".strtbtn");
+
 stopButton.addEventListener('click', ()=>{
-    console.log("Clicked")
-    content.classList.add("active");
+    console.log("stopped")
+    liveStreaming.style.display = "none";
+    streaming.style.display = "block";
+})
+
+strtButton.addEventListener('click', ()=>{
+    console.log("started");
+    streaming.style.display = "none";
+    liveStreaming.style.display = "flex";
 })
