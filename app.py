91921347997aba4cdb6adb94c47dbb0772d0169a
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-04-20 15:18:18 +0600

    hand detection content done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, render_template
 import cv2
 import faceTracker as ft
+import handTracker as ht
 
 
 app = Flask(__name__)
@@ -9,21 +10,51 @@ app = Flask(__name__)
 def index_page():
     return render_template('index.html')
 
+#Face detection front end
 @app.route("/face_detection")
-def face():
+def face_tracking():
     return render_template('facetracking.html')
 
+#face detetcion module
 def face_detection():
-    tracker = ft.faceDetector()
+    face_tracker = ft.faceDetector()
     cam = cv2.VideoCapture(0)
     while True:
         ret, frame = cam.read()
         if ret:
-            _ , frame = tracker.findFaces(frame, drawP=False)
+            _ , frame = face_tracker.findFaces(frame, drawP=False)
             frame = cv2.imencode('.jpg', frame)[1].tobytes()
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         else:
             break
+
+#face detection streaming
+@app.route("/faceTracking")
+def faceTracking_streaming():
+    return Response(face_detection(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#Hand detection front end
+@app.route("/hand_detection")
+def hand_tracking():
+    return render_template("handtracking.html")
+#face detection module
+def hand_detection():
+    hand_tracker = ht.handTracker()
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cam.read()
+        if ret:
+            hands = hand_tracker.findHands(frame)
+            frame = cv2.imencode('.jpg', frame)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            break
+#hand detection streaming
+@app.route("/handtracking")
+def handTracking_streaming():
+    return Response(hand_detection(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#original video streaming
 def original_face():
     cam = cv2.VideoCapture(0)
     while True:
@@ -34,9 +65,7 @@ def original_face():
         else:
             break
         
-@app.route("/faceTracking")
-def faceTracking_streaming():
-    return Response(face_detection(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @app.route("/originalface")
 def originalFace_streaming():
     return Response(original_face(),mimetype='multipart/x-mixed-replace; boundary=frame')
