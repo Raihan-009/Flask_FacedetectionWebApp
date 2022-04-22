2d8c465e8641d4fb706161d05e9e3df423d0cc65
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-04-22 09:21:07 +0600

    finger counting project is not showing

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import cv2
 import faceTracker as ft
 import handTracker as ht
 import meshTracker as mt
+import fingerCounter as fc
 
 
 app = Flask(__name__)
@@ -38,6 +39,7 @@ def faceTracking_streaming():
 @app.route("/hand_detection")
 def hand_tracking():
     return render_template("handtracking.html")
+
 #face detection module
 def hand_detection():
     hand_tracker = ht.handTracker()
@@ -50,6 +52,7 @@ def hand_detection():
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         else:
             break
+
 #hand detection streaming
 @app.route("/handtracking")
 def handTracking_streaming():
@@ -59,6 +62,7 @@ def handTracking_streaming():
 @app.route("/mesh_detection")
 def mesh_tracking():
     return render_template("meshtracking.html")
+
 #mesh detection module
 def mesh_detection():
     mesh_tracker = mt.MeshDetection()
@@ -71,12 +75,52 @@ def mesh_detection():
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         else:
             break
+
 #mesh detection streaming
 @app.route("/meshtracking")
 def meshtracking_streaming():
     return Response(mesh_detection(),mimetype='multipart/x-mixed-replace; boundary=frame')    
 
+#finger Counter front end
+@app.route("/finger_counter")
+def counted_finger_tracking():
+    return render_template("fingercounter.html")
 
+#finger counting module
+def finger_counting():
+    hand_tracker = ht.handTracker()
+    finger_counter = fc.FingerCounter()
+
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cam.read()
+        cv2.rectangle(frame, (10,10), (250,30), (255,255,255), cv2.FILLED)
+        cv2.putText(frame, 'Project Finger Counting', (15,25), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255,0,0), 1)
+        if ret:
+            hands = hand_tracker.findHands(frame)
+            if hands:
+                oneHand = hands[0]
+                first_five = finger_counter.fingerOrientation(oneHand)
+
+                if (len(hands) == 2):
+                    secondHand = hands[1]
+                    second_five = finger_counter.fingerOrientation(secondHand)
+                    cv2.rectangle(frame, (10,50), (245,200), (55,245,10), cv2.FILLED)
+                    cv2.putText(frame, str(first_five + second_five), (65,175), cv2.FONT_HERSHEY_COMPLEX, 4, (0,0,0), 20)
+                    yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                else:
+                    cv2.rectangle(frame, (40,50), (200,200), (55,245,10), cv2.FILLED)
+                    cv2.putText(frame, str(first_five), (75,175), cv2.FONT_HERSHEY_COMPLEX, 4, (0,0,0), 20)
+                    yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            break
+
+#counted finger streaming
+@app.route("/countedfingerStreaming")
+def fingerCounting_streaming():
+    return Response(finger_counting(),mimetype='multipart/x-mixed-replace; boundary=frame')   
+    
 #original video streaming
 def original_face():
     cam = cv2.VideoCapture(0)
